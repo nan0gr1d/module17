887de3958ebc08be55c7487f13ae3d04c5b3913b
Urban <prestofan@gmail.com>
2024-10-25 17:20:44 +0300

    Основа app построена

---
+++ app/main.py
@@ -0,0 +1,15 @@
+#  main
+
+from fastapi import FastAPI
+from routers import user, task
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+app.include_router(task.router)
+app.include_router(user.router)
+
+
